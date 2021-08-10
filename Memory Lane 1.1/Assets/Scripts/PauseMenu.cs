659eb203be4bb8a3c5872dd4df9fb5c0d6e42076
idkwho182 <88501647+idkwho182@users.noreply.github.com>
2021-08-10 13:28:20 -0400

    First commit for the love of GOD

---
+++ Memory Lane 1.1/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // if user presses the P key while the game is running it will pause. If the game is already paused the p key will resume the game
+    void Update() { 
+        if (Input.GetKeyDown(KeyCode.P)){
+
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Resume()
+    {
+        // This unpauses the game and sets the speed of the game back to normal 
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        // Pauses the game by setting the speed of the game to 0 
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        // Quits out of the application
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
